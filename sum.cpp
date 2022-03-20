b895561f9e748978565f77be104b51c49fc8ad79
AYAM-BLESS <blessbissong@gmail.com>
2022-03-19 20:43:46 -0700

    sum of variables

---
+++ sum.cpp
@@ -0,0 +1,17 @@
+//done Ayamba Bless
+//simple sum function
+#include<iostream>
+using namespace std;
+
+double sum(double x, double y, double z)
+{
+    return x + y + z;
+}
+int main()
+{
+    double a = 1.3;
+    double b = 3.3;
+    double c = 4.6;
+    cout << sum(a, b, c);
+    return 0;
+}
