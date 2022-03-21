14e56f047371e63004dce4cb0da91d244db18149
AYAM-BLESS <blessbissong@gmail.com>
2022-03-20 21:04:02 -0700

    using class

---
+++ class.cpp
@@ -0,0 +1,16 @@
+//done by Ayamba Bless
+//Class
+#include<iostream>
+using namespace std;
+class BankAccount{
+    public:
+    void sayHi(){
+        cout << "Hi" << endl;
+    }
+};
+
+int main()
+{
+    BankAccount test;
+    test.sayHi();
+}
