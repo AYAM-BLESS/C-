1034d08d55c124339df70b155b8f07e7823e2151
AYAM-BLESS <blessbissong@gmail.com>
2022-03-21 20:43:34 -0700

    source and header files

---
+++ source&headerFile2.cpp
@@ -0,0 +1,8 @@
+//done by Ayamba Bless
+//To use our classes in our main we need to include the header file
+#include <iostream>
+#include "MyClass.h"
+using namespace std;
+int main(){
+    MyClass obj;
+}
