ee5ee67d94d0f991b46a6408c1fac3633f29300c
AYAM-BLESS <blessbissong@gmail.com>
2022-03-21 20:35:33 -0700

    source and header files

---
+++ source&headerFile.cpp
@@ -0,0 +1,15 @@
+//done by Ayamba Bless
+//the header file holds the function declaration and variables declarations.
+//It currently includes a template for our new MyClass class with one default constructor.
+//MyClass.h
+#ifndef MYCLASS_H
+#define MYCLASS_H
+
+class MyClass
+{
+    public:
+    MyClass();
+    protected:
+    private:
+};
+#endif // MYCLASS_H
