75a831fa7924100e32b59163f435a77a1019b58c
AYAM-BLESS <blessbissong@gmail.com>
2022-03-21 20:40:04 -0700

    Scope resolution operator

---
+++ scopeResolutionOperator.cpp
@@ -0,0 +1,9 @@
+//done by Ayamba Bless
+//Scope resolution operator
+//The double colon is the scope resolution operator and its used for construction definition
+//the scope resolution operator is used to define a particular class member functions which have already been declared
+#include "MyClass.h"
+MyClass::MyClass()
+{
+    //ctor
+}
