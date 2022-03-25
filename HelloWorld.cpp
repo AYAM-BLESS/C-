f05f178e9e57d5f905530408bdab98e0c01274ff
AYAM-BLESS <blessbissong@gmail.com>
2022-03-24 21:22:34 -0700

    displaying Hello World using class

---
+++ HelloWorld.cpp
@@ -0,0 +1,17 @@
+//done by Ayamba Bless
+//Hello World using class
+#include<iostream>
+using namespace std;
+//creating a class
+class message{
+    public:
+    void display(){
+        cout<<"Hello World\n";
+    }
+};
+ int main()
+ {
+    message c; //Creating an object
+    c.display(); // calling the funtion
+    return 0;
+ }
